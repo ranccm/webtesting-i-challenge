d45579e040d699b7f993959324927b2c7c8e7147
ranccm <ranimnapier@gmail.com>
2020-06-18 13:54:52 -0700

    fail method failing

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,18 +5,55 @@ module.exports = {
   get,
 };
 
+
+//returns new item with
+//enhancement incresed by one, unchanged if enhancement is at 20.
 function succeed(item) {
-  return { ...item };
-}
+  if (item.enhancement < 20) {
+    return {
+      ...item,
+      enhancement: item.enhancement + 1
+    }
+  } else {
+    return {
+      ...item
+    }
+  }}
 
+//if item.enhancement < 15, item.durability -5 *if the items enhancementlevel is less than 15
+//if item.enhancement >= 15, item.durability -10 * if the enhancement is 15 or more
+//if item.enhancement > 16, item.enhancement -1 * if enhancement is more than 16
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    return {
+      ...item,
+      durability: item.durability -5
+    } 
+  } else if (item.enhancement >= 15) {
+    return {
+      ...item,
+      durability: item.durability -10
+    }
+  } else if (item.enhancement > 16){
+    return {
+      ...item,
+      durability: item.durability -10,
+      enhancement: item.enhancement -1
+    }
+  } 
 }
 
+//returns new item with durability restored to 100
 function repair(item) {
-  return { ...item };
+  return {
+    ...item,
+    durability: 100,
+  };
 }
 
+
+
+//stretch
 function get(item) {
   return { ...item };
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,66 @@
-const enhancer = require('./enhancer.js');
+const {repair, succeed, fail} = require('./enhancer.js');
+
 // test away!
+
+describe('enhancer', function(){
+        it("should run tests using it()", function() {
+            expect(true).toBe(true);
+        })
+        
+        describe('test repair method', function(){
+            it('returns new item with durability at 100', function(){
+                const item = {
+                    name: 'sword',
+                    durability: 75,
+                    enhancement: 0
+                }
+                const repairedItem = repair(item);
+                expect(repairedItem.durability).toEqual(100);
+            })
+        })
+
+        describe('test succeed method', function(){
+            it('returns new item with +1 enhancement', function() {
+                const item = {
+                    name: 'sword',
+                    durability: 75,
+                    enhancement: 0
+                }
+                const enhanced = succeed(item);
+                expect(enhanced.enhancement).toEqual(1);
+               
+            })
+            it("durability remains unchanged if = 20", function() {
+                const item = {
+                    name: 'sword',
+                    durability: 75,
+                    enhancement: 20
+                }
+                const enhanced = succeed(item);
+                expect(enhanced.enhancement).toEqual(20);
+            })
+        })
+        describe("test fail method", function() {
+            it('if enhancement is less than 15, subtracts 5 ...levels? from durability', function() {
+                const item = {
+                    name: 'sword',
+                    durability: 75,
+                    enhancement: 10
+                }
+                const newItem = fail(item);
+                expect(newItem.durability).toEqual(70)
+            })
+        })
+        it('if enhancment is 15+, subtract 10 from durability', function(){
+            const item = {
+                name: 'sword',
+                durability: 75,
+                enhancement: 17
+            }
+            const newItem = fail(item);
+            expect(newItem.durability).toEqual(65)
+            // expect(newItem.enhancement).toEqual(16)
+        })
+       
+ 
+})
